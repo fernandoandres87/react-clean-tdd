a37f5ab5b559e76031ec8b7d3b35e25eb0ca8fb5
Fernando Lopez <fernandol@fktech.net>
2021-05-08 00:20:21 -0300

    feat: add configuration files

---
+++ src/User/data/usecases/authentication/remote-authentication.spec.ts
@@ -0,0 +1,12 @@
+import { HttpPostClientSpy } from '../../test/mock-http-client'
+import { RemoteAuthentication } from './remote-authentication'
+
+describe('RemoteAuthentication', () => {
+  test('should call HttpPostClient with correct URL', async () => {
+    const httpPostClientSpy = new HttpPostClientSpy()
+    const url = 'any_url'
+    const sut = new RemoteAuthentication(url, httpPostClientSpy)
+    await sut.auth()
+    expect(httpPostClientSpy.url).toBe(url)
+  })
+})
