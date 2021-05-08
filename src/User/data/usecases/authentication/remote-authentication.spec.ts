88eaf33eb6d187937e33ad3795f6788aaddafd02
Fernando Lopez <fernandol@fktech.net>
2021-05-08 02:17:39 -0300

    refactor: refactor code

--- src/User/data/usecases/authentication/remote-authentication.spec.ts
+++ src/User/data/usecases/authentication/remote-authentication.spec.ts
@@ -3,10 +3,10 @@ import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { HttpPostClientSpy } from '@/data/test/mock-http-client'
 import { mockAuthentication } from '@/domain/test/mock-authentication'
 import { InvalidCredentialError } from '@/domain/error/invalid-credentials-error'
-import faker from 'faker'
 import { UnexpectedError } from '@/domain/error/unexpected-error'
 import { AuthenticationParams } from '@/domain/usecases/authentication'
 import { AccountModel } from '@/domain/models/account-model'
+import faker from 'faker'
 
 type SutTypes = {
   sut: RemoteAuthentication
