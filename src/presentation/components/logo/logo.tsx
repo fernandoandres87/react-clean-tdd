97d455fbc260db0a6cf9f12ae3d893ccc91b57ef
Fernando Lopez <fernandol@fktech.net>
2021-05-16 13:01:26 -0300

    refactor move logo to a separate a file

---
+++ src/presentation/components/logo/logo.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Logo: React.FC = () => {
+  return (
+    <div>
+      {/* Aca debe ir la imagen del logo y un nombre */}
+      <h1>App test DDD</h1>
+    </div>
+  )
+}
+
+export default Logo
