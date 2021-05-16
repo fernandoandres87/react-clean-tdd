60f9f24dbe2a4b4bbd3c1f1d276ee2c7c0ed332f
Fernando Lopez <fernandol@fktech.net>
2021-05-16 16:52:36 -0300

    feat: ensure Login starts with input with required state

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -3,24 +3,23 @@ import Styles from './login.styles.scss'
 import { Footer, Input, LoginHeader, FormStatus } from '@/presentation/components'
 import Context from '@/presentation/contexts/form/form-context'
 
-type StateProps = {
-  isLoading: boolean
-  errorMessage: string
-
-}
 const Login: React.FC = () => {
-  const [state] = useState<StateProps>({
-    isLoading: false,
-    errorMessage: ''
+  const [state] = useState({
+    isLoading: false
+  })
+  const [errorState] = useState({
+    email: 'Campo obligatorio',
+    password: 'Campo obligatorio',
+    main: ''
   })
   return (
     <div className= { Styles.login }>
       <LoginHeader />
-      <Context.Provider value={state}>
+      <Context.Provider value={{ state, errorState }}>
         <form className= { Styles.form }>
           <h2> Login </h2>
-          <Input type="email" name="email" placeholder="Escriba su email"/>
-          <Input type="password" name="password" placeholder="Escriba su password"/>
+          <Input type="email" name="email" placeholder="Escriba su email" />
+          <Input type="password" name="password" placeholder="Escriba su password" />
           <button data-testid="submit" disabled className={Styles.submit} type='submit'> Entrar </button>
           <span className={Styles.link}>Crear Cuenta</span>
           <FormStatus />
