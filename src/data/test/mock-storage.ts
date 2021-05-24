86b94e4ed3a9669b36d1976d52e0c5433666adae
Fernando Lopez <fernandol@fktech.net>
2021-05-24 19:46:50 -0300

    refactor: rename SetStorageSpy to SetStorageMock

--- src/data/test/mock-storage.ts
+++ src/data/test/mock-storage.ts
@@ -1,6 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { SetStorage } from '../protocols/cache/set-storage'
 
-export class SetStorageSpy implements SetStorage {
+export class SetStorageMock implements SetStorage {
   key: string
   value: any
 
